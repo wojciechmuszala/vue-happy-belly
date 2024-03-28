3e4d8a8eb4b1988e5c5e05d81c82a1600d540f77
Wojciech Muszala <wojciech.muszala@gmail.com>
2024-03-28 20:40:04 +0100

    Feature: Add burger animation

--- src/components/layout/TheNavigation.vue
+++ src/components/layout/TheNavigation.vue
@@ -6,27 +6,32 @@
       <base-button class="btn-primary">Sign in</base-button>
       <div
         @click="toggleMenu"
-        class="flex flex-col items-end"
+        class="relative flex w-8 flex-col items-end gap-y-1.5"
         :class="{ open: showMenu }"
       >
         <span
-          class="h-1 w-5 bg-light-yellow transition-all duration-300"
-        ></span>
-        <span
-          class="my-1.5 h-1 w-8 bg-light-yellow transition-all duration-300"
-        ></span>
-        <span
-          class="h-1 w-8 bg-light-yellow transition-all duration-300"
+          v-for="n in 3"
+          :key="n"
+          class="h-1 bg-light-yellow transition-all duration-500"
+          :class="{
+            'w-3/4': n === 1,
+            'w-full': n === 2 || n === 3,
+            'translate-x-full opacity-0': n === 1 && showMenu,
+            'translate-y-[3.5px] rotate-45': n === 2 && showMenu,
+            'translate-y-[-7px] -rotate-45': n === 3 && showMenu,
+          }"
         ></span>
       </div>
       <nav
-        class="bg-inherit absolute bottom-0 left-0 z-[-10] flex w-full flex-col justify-center p-5 shadow-lg transition-all duration-300 ease-in-out"
-        :class="[showMenu ? 'translate-y-full' : '']"
+        class="bg-inherit absolute bottom-0 left-0 z-[-10] flex w-full flex-col justify-center p-5 shadow-lg transition-all duration-500 ease-in-out"
+        :class="{ 'translate-y-full': showMenu }"
       >
-        <router-link to="">Link 1</router-link>
-        <router-link to="">Link 2</router-link>
-        <router-link to="">Link 3</router-link>
-        <router-link to="">Link 4</router-link>
+        <router-link
+          v-for="menuItem in menuList"
+          :key="menuItem"
+          :to="menuItem.link"
+          >{{ menuItem.title }}</router-link
+        >
       </nav>
     </menu>
   </header>
@@ -40,13 +45,24 @@ const showMenu = ref(false);
 const toggleMenu = () => {
   showMenu.value = !showMenu.value;
 };
+const menuList = [
+  {
+    title: "Link 1",
+    link: "#",
+  },
+  {
+    title: "Link 2",
+    link: "#",
+  },
+  {
+    title: "Eueue",
+    link: "#",
+  },
+  {
+    title: "Link 4",
+    link: "#",
+  },
+];
 </script>
 
-<style scoped>
-/* .open {
-  @apply [&>*:nth-child(1)]:opacity-0 [&>span:nth-child(1)]:translate-x-full;
-} */
-.open span:nth-child(1) {
-  @apply translate-x-full opacity-0;
-}
-</style>
+<style scoped></style>
