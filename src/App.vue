10009cff320b1e224b3b7db4cbb8e83f0387fccf
Wojciech Muszala <wojciech.muszala@gmail.com>
2024-04-02 21:12:17 +0200

    Feature: Add desktop navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div
-    class="flex min-h-screen flex-col overflow-hidden bg-light-yellow font-quicksand text-dark-blue"
+    class="flex min-h-screen flex-col overflow-hidden font-quicksand text-dark-blue"
   >
-    <the-navigation></the-navigation>
-    <main-content></main-content>
+    <the-navigation
+      @get-navbar-state="handleNavbarState"
+      :expanded-style-value="expandedStyleValue"
+      :not-expanded-style-value="notExpandedStyleValue"
+    ></the-navigation>
+    <main-content
+      class="ml-52 transition-all duration-300 ease-in-out"
+      :class="
+        isNavbarExpanded ? navbarStyles.expanded : navbarStyles.notExpanded
+      "
+    ></main-content>
   </div>
 </template>
 
 <script setup>
 import TheNavigation from "./components/layout/TheNavigation";
 import MainContent from "./components/layout/MainContent";
+import { ref } from "vue";
+
+const isNavbarExpanded = ref(false);
+const handleNavbarState = (navbarState) => {
+  isNavbarExpanded.value = navbarState;
+};
+
+const expandedStyleValue = 52;
+const notExpandedStyleValue = 32;
+const navbarStyles = {
+  expanded: `md:ml-${expandedStyleValue}`,
+  notExpanded: `md:ml-${notExpandedStyleValue}`,
+};
 </script>
 
 <style></style>
--- src/components/layout/TheNavigation.vue
+++ src/components/layout/TheNavigation.vue
@@ -1,30 +1,34 @@
 <template>
-  <header class="fixed left-0 top-0 w-full bg-dark-blue">
+  <header
+    class="md:stati fixed left-0 top-0 w-full bg-dark-blue transition-all duration-300 ease-in-out md:h-svh"
+    :class="isNavbarExpanded ? navbarStyles.expanded : navbarStyles.notExpanded"
+  >
     <menu
-      class="bg-inherit flex w-full items-center justify-between p-5 text-light-yellow"
+      class="bg-inherit flex w-full items-center justify-between p-5 text-light-yellow md:flex-col md:p-2"
     >
+      <app-logo class="mb-5 hidden w-12 md:block"></app-logo>
       <base-button class="btn-primary">Sign in</base-button>
       <div
         @click="toggleMenu"
-        class="relative flex w-8 flex-col items-end gap-y-1.5"
-        :class="{ open: showMenu }"
+        class="relative flex w-8 flex-col items-end gap-y-1.5 md:hidden"
+        :class="{ open: isMenuVisible }"
       >
         <span
           v-for="n in 3"
           :key="n"
-          class="h-1 bg-light-yellow transition-all duration-500"
+          class="h-1 bg-light-yellow transition-all duration-300"
           :class="{
             'w-3/4': n === 1,
             'w-full': n === 2 || n === 3,
-            'translate-x-full opacity-0': n === 1 && showMenu,
-            'translate-y-[3.5px] rotate-45': n === 2 && showMenu,
-            'translate-y-[-7px] -rotate-45': n === 3 && showMenu,
+            'translate-x-full opacity-0': n === 1 && isMenuVisible,
+            'translate-y-[3.5px] rotate-45': n === 2 && isMenuVisible,
+            'translate-y-[-7px] -rotate-45': n === 3 && isMenuVisible,
           }"
         ></span>
       </div>
       <nav
         class="bg-inherit absolute bottom-0 left-0 z-[-10] flex w-full flex-col justify-center p-5 shadow-lg transition-all duration-500 ease-in-out"
-        :class="{ 'translate-y-full': showMenu }"
+        :class="{ 'translate-y-full': isMenuVisible }"
       >
         <router-link
           v-for="menuItem in menuList"
@@ -33,18 +37,18 @@
           >{{ menuItem.title }}</router-link
         >
       </nav>
+      <div class="color-red" @click="toggleNavbar">
+        {{ isNavbarExpanded ? "<<<" : ">>>" }}
+      </div>
     </menu>
   </header>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, defineProps, defineEmits } from "vue";
 import BaseButton from "../base/BaseButton";
+import AppLogo from "@/components/svg/AppLogo";
 
-const showMenu = ref(false);
-const toggleMenu = () => {
-  showMenu.value = !showMenu.value;
-};
 const menuList = [
   {
     title: "Link 1",
@@ -63,6 +67,28 @@ const menuList = [
     link: "#",
   },
 ];
+
+const props = defineProps(["expandedStyleValue", "notExpandedStyleValue"]);
+
+const navbarStyles = {
+  expanded: `md:w-${props.expandedStyleValue}`,
+  notExpanded: `md:w-${props.notExpandedStyleValue}`,
+};
+
+const isNavbarExpanded = ref(false);
+const toggleNavbar = () => {
+  isNavbarExpanded.value = !isNavbarExpanded.value;
+  sendNavbarExpandInfo();
+};
+
+const isMenuVisible = ref(false);
+const toggleMenu = () => {
+  isMenuVisible.value = !isMenuVisible.value;
+};
+
+const emit = defineEmits(["getNavbarState"]);
+const sendNavbarExpandInfo = () =>
+  emit("getNavbarState", isNavbarExpanded.value);
 </script>
 
 <style scoped></style>
