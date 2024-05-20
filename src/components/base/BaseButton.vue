926317484cea27791a4b50dadfd85f9f5823f64a
Wojciech Muszala <wojciech.muszala@gmail.com>
2024-05-20 18:24:39 +0200

    F: Handle router-link, add active effect and tertiary btn styles in BaseButton

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,22 +1,42 @@
 <template>
-  <a v-if="type === 'link'" class="inline-block rounded-full px-4 py-2.5">
+  <a
+    v-if="mode === 'link'"
+    class="flex items-center gap-3 rounded-full px-4 py-2.5 active:scale-95"
+    :class="[`btn-${type}`, icon ? 'justify-between' : 'justify-center']"
+  >
     <slot></slot>
+    <font-awesome-icon v-if="icon" :icon="icon" />
   </a>
-  <button v-else class="inline-block rounded-full px-4 py-2.5">
-    <slot></slot>
+  <router-link
+    v-else-if="mode === 'router-link'"
+    class="flex items-center gap-3 rounded-full px-4 py-2.5 active:scale-95"
+    :class="[`btn-${type}`, icon ? 'justify-between' : 'justify-center']"
+  >
+    <slot />
+  </router-link>
+  <button
+    v-else
+    class="flex items-center gap-3 rounded-full px-4 py-2.5 active:scale-95"
+    :class="[`btn-${type}`, icon ? 'justify-between' : 'justify-center']"
+  >
+    <slot />
+    <font-awesome-icon v-if="icon" :icon="icon" />
   </button>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
-defineProps(["type"]);
+defineProps(["mode", "type", "icon"]);
 </script>
 
 <style scoped>
 .btn-primary {
-  @apply text-normal-orange bg-dark-blue font-semibold;
+  @apply text-normal-orange hover:bg-normal-orange border border-dark-blue bg-dark-blue font-semibold transition-all duration-300 hover:text-dark-blue;
 }
 .btn-secondary {
-  @apply bg-normal-orange font-semibold text-dark-blue;
+  @apply bg-normal-orange hover:text-normal-orange border-normal-orange border font-semibold text-dark-blue transition-all duration-300 hover:bg-dark-blue;
+}
+.btn-tertiary {
+  @apply text-white hover:bg-normal-orange border border-dark-blue bg-dark-blue font-semibold transition-all duration-300 hover:text-dark-blue;
 }
 </style>
