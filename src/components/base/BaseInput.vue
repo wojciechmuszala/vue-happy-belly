c632c0e9eb449acbb997e31778736a9b3fa2f18f
Wojciech Muszala <wojciech.muszala@gmail.com>
2024-05-24 19:06:45 +0200

    F/B: Change BaseInput select (fix value handling), add and handle unit input

--- src/components/base/BaseInput.vue
+++ src/components/base/BaseInput.vue
@@ -26,7 +26,7 @@
     />
     <label
       :for="id"
-      class="peer-focus:bg-white peer=focus:rounded pointer-events-none absolute inset-y-1/2 left-5 h-max -translate-y-1/2 transition-all duration-200 peer-focus:-top-1 peer-focus:left-5 peer-focus:h-max peer-focus:px-1 peer-focus:py-0 peer-focus:text-xs peer-focus:text-dark-blue"
+      class="peer-focus:bg-white pointer-events-none absolute inset-y-1/2 left-5 h-max -translate-y-1/2 transition-all duration-200 peer-focus:-top-1 peer-focus:left-5 peer-focus:h-max peer-focus:rounded peer-focus:px-1 peer-focus:py-0 peer-focus:text-xs peer-focus:text-dark-blue"
       :class="[
         labelClass,
         modelValue
@@ -41,51 +41,75 @@
       class="absolute right-4 top-1/2 -translate-y-1/2"
     />
   </div>
-  <details
-    ref="detailsElement"
-    v-else-if="type === 'select'"
-    class="min-w-60 rounded-lg border border-dark-blue [&_summary:after]:content-['▼'] [&_summary:after]:open:content-['▲']"
-  >
-    <summary
-      class="relative z-10 flex gap-3 after:absolute after:top-1/2 after:flex after:h-full after:w-full after:-translate-y-1/2 after:items-center after:justify-end after:pr-3 after:text-right"
+  <div v-else-if="type === 'select'" class="relative min-w-60">
+    <details
+      ref="detailsElement"
+      class="bg-white rounded-full border border-dark-blue [&_summary:after]:content-['▼'] [&_summary:after]:open:content-['▲']"
+      :class="{
+        'sm:rounded-none sm:border-r-0': noRounded === 'both',
+        'sm:rounded-l-full sm:rounded-r-none sm:border-r-0':
+          noRounded === 'right',
+        'sm: rounded-l-none sm:rounded-r-full sm:border-l-0':
+          noRounded === 'left',
+      }"
     >
-      <input
-        type="radio"
-        :name="id"
-        id="default"
-        :title="defaultOption"
-        :value="modelValue"
-        class="hidden w-full appearance-none p-3 pr-9 font-semibold before:content-[attr(title)] checked:inline-block"
-        checked="true"
-      />
-      <input
-        v-for="option in options"
-        :key="option"
-        type="radio"
-        :name="id"
-        :id="option"
-        :title="option"
-        :value="modelValue"
-        class="hidden w-full appearance-none p-3 pr-9 font-semibold before:content-[attr(title)] checked:inline-block"
-      />
-    </summary>
-    <ul class="[&_li:first-child]:border-t [&_li:last-child]:border-none">
-      <li
-        class="hover:bg-gray-100 border-b border-dark-blue"
-        @click="handleCloseSelect"
+      <summary
+        class="bg-inherit group relative z-10 flex gap-3 after:absolute after:top-1/2 after:flex after:h-full after:w-full after:-translate-y-1/2 after:items-center after:justify-end after:pr-3 after:text-right"
       >
-        <label for="default" class="block px-3 py-1">{{ defaultOption }}</label>
-      </li>
-      <li
-        v-for="option in options"
-        :key="option"
-        @click="handleCloseSelect"
-        class="hover:bg-gray-100 border-b border-b-dark-blue"
+        <label
+          :for="id"
+          class="peer-focus:bg-white pointer-events-none absolute inset-y-1/2 left-5 h-max -translate-y-1/2 transition-all duration-200 peer-focus:-top-1 peer-focus:left-5 peer-focus:h-max peer-focus:rounded peer-focus:px-1 peer-focus:py-0 peer-focus:text-xs peer-focus:text-dark-blue"
+          :class="[
+            labelClass,
+            modelValue
+              ? 'bg-white opacity-1 -top-1 rounded px-1 py-0 text-xs text-dark-blue'
+              : 'opacity-0',
+          ]"
+          >{{ label }}</label
+        >
+        <input
+          type="radio"
+          :name="id"
+          id="default"
+          :title="label"
+          value="modelValue"
+          class="hidden w-full appearance-none py-2.5 pl-5 pr-9 font-semibold text-dark-blue/[0.2] before:content-[attr(title)] checked:inline-block"
+          checked="true"
+        />
+        <input
+          v-for="option in options"
+          @change="handleChange(option)"
+          :key="option"
+          type="radio"
+          :name="id"
+          :id="option"
+          :title="option"
+          :value="option"
+          class="hidden w-full appearance-none py-2.5 pl-5 pr-9 before:content-[attr(title)] checked:inline-block"
+        />
+      </summary>
+      <ul
+        class="bg-white absolute left-0 top-full w-full border border-t-0 border-dark-blue [&_li:last-child]:border-none"
       >
-        <label :for="option" class="block px-3 py-1"> {{ option }}</label>
-      </li>
-    </ul>
-  </details>
+        <li
+          class="hover:bg-gray-100 border-b border-t-0 border-dark-blue"
+          @click="handleCloseSelect"
+        >
+          <label for="default" class="block px-3 py-1">
+            {{ label }}
+          </label>
+        </li>
+        <li
+          v-for="option in options"
+          :key="option"
+          @click="handleCloseSelect"
+          class="hover:bg-gray-100 border-b border-b-dark-blue"
+        >
+          <label :for="option" class="block px-3 py-1"> {{ option }}</label>
+        </li>
+      </ul>
+    </details>
+  </div>
   <div v-else-if="type === 'file'">
     <input :id="id" :type="type" class="hidden" />
     <label
@@ -117,6 +141,7 @@ const props = defineProps([
   "modelModifiers",
   "validateFunction",
   "onInput",
+  "onChange",
   "onBlur",
   "options",
   "defaultOption",
@@ -137,18 +162,21 @@ const typeAttributes = (() => {
 const emit = defineEmits(["update:modelValue"]);
 
 const firstCharacterTyped = ref(false);
-const emitInputValue = (event) => {
+const emitInputValue = (inputValue) => {
   firstCharacterTyped.value = true;
   props.validateFunction && props.validateFunction();
-  let value = event.target.value;
 
   //place to add custom model modifiers
   /* if (modelModifiers[...]) {
     value = value.replace(...);
   } */
-
-  if (props.type === "numner") {
-    value = value.toFixed(2);
+  
+  let value;
+  if (props.type === "select") {
+    value = inputValue;
+  } else {
+    const event = inputValue;
+    value = event.target.value;
   }
 
   emit("update:modelValue", value);
@@ -159,11 +187,14 @@ const checkIsInputValid = () =>
     ? props.validateFunction && props.validateFunction()
     : true;
 
-const handleInput = (event) => {
-  props.onInput != undefined && emitInputValue(event);
+const handleInput = (inputValue) => {
+  props.onInput != undefined && emitInputValue(inputValue);
 };
-const handleBlur = (event) => {
-  props.onBlur != undefined && emitInputValue(event);
+const handleChange = (inputValue) => {
+  props.onChange != undefined && emitInputValue(inputValue);
+};
+const handleBlur = (inputValue) => {
+  props.onBlur != undefined && emitInputValue(inputValue);
 };
 const detailsElement = ref();
 const handleCloseSelect = () => {
--- src/views/RecipeAddView.vue
+++ src/views/RecipeAddView.vue
@@ -1,3 +1,7 @@
+<!-- TODO - Add validation fct to ingredients form  -->
+<!-- TODO - Add 'method of preparing the recipe' section -->
+<!-- TODO - Refactor and handle main form -->
+
 <template>
   <div>
     <h1 class="py-5 text-center text-7xl font-semibold">Add recipe</h1>
@@ -26,31 +30,46 @@
           class="flex max-w-2xl flex-col gap-3 sm:flex-row sm:gap-0"
         >
           <base-input
+            ref="startIngeredientInput"
             onInput
             type="text"
             label="Add ingredient"
             no-rounded="right"
-            v-model="ingredientName"
-            class="grow-1 basis-full"
+            v-model="ingredient.name"
+            class="sm:basis-1/2"
           />
           <base-input
             onInput
             type="number"
             label="Qantity"
             no-rounded="both"
-            v-model="ingredientQuantity"
-            class="basis-1/3"
+            v-model="ingredient.quantity"
+            class="sm:basis-1/5"
+          />
+          <base-input
+            onChange
+            type="select"
+            id="unit"
+            label="Unit"
+            :options="['g', 'ml', 'tsp', 'tbsp', 'cup']"
+            no-rounded="both"
+            v-model="ingredient.unit"
+            class="!min-w-0 sm:basis-1/5"
           />
           <base-button
             type="primary"
             :icon="['fas', 'plus']"
             no-rounded="left"
-            class="shrink-1 ml-auto w-1/4 sm:w-auto [&_div]:justify-center"
+            class="sm:basis-[10%]"
           />
         </form>
-        <ul v-if="ingredientList.length > 0">
+        <ul
+          v-if="ingredientList.length > 0"
+          class="mt-5 flex list-disc flex-col gap-2 pl-5"
+        >
+          <li>carrot - 3tsp</li>
           <li v-for="ingredient in ingredientList" :key="ingredient">
-            {{ ingredient }}
+            {{ ingredient.name }} - {{ ingredient.quantity + ingredient.unit }}
           </li>
         </ul>
       </div>
@@ -59,22 +78,29 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, shallowRef } from "vue";
 
-// TODO: Change igredient to one reactive object
 const recipeName = ref("");
-const ingredientName = ref("");
-const ingredientQuantity = ref("");
+const ingredient = reactive({
+  name: null,
+  quantity: null,
+  unit: null,
+});
 const ingredientList = ref([]);
+const startIngeredientInput = ref(null);
 const addIngredient = () => {
-  if (!ingredientList.value.includes(ingredientName.value)) {
-    ingredientList.value.push([ingredientName.value, ingredientQuantity.value]);
+  const newIngredient = shallowRef({ ...ingredient });
+  if (!ingredientList.value.includes(ingredient.name)) {
+    ingredientList.value.push(newIngredient.value);
+    for (let key in ingredient) {
+      ingredient[key] = null;
+    }
+    startIngeredientInput.value.$el.querySelector("input").focus();
   } else {
     // TODO: Add validation fct and info
     console.log("Already on list!");
   }
 };
-
 </script>
 
 <style lang="scss" scoped></style>
